2373e277171970af9d129669302d38967f4e821a
Steve Clark <steve.a.clark48@gmail.com>
2021-08-09 14:34:32 -0500

    deleted import that was not needed

--- music_library/music/views.py
+++ music_library/music/views.py
@@ -6,7 +6,6 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
-from music_library.music import serializers
 
 
 # Create your views here.
