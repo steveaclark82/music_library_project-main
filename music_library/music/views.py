8b82434cda9607118bafab6d1fa424b0badf10cb
Steve Clark <steve.a.clark48@gmail.com>
2021-08-09 13:53:52 -0500

    created song detail class

--- music_library/music/views.py
+++ music_library/music/views.py
@@ -1,9 +1,12 @@
+from django.http.response import Http404
 from django.shortcuts import render
 from .models import Song
 from .serializers import SongSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+
+from music_library.music import serializers
 
 
 # Create your views here.
@@ -20,3 +23,17 @@ class SongList(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    class SongDetails(APIView):
+        def get_object(self, pk):
+            try:
+              return Song.objects.get(pk=pk)
+            except Song.DoesNotExist:
+                raise Http404
+            
+        def get(self, request, pk):
+            song = self.get_object(pk)
+            serializer = SongSerializer(song)
+            return Response(serializer.data)
+        
+        
