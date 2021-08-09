484532a6da3b5211c7b8c810d21c453a47412a9a
Steve Clark <steve.a.clark48@gmail.com>
2021-08-09 14:45:12 -0500

    fixed indentation

--- music_library/music/views.py
+++ music_library/music/views.py
@@ -24,27 +24,27 @@ class SongList(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
-    class SongDetail(APIView):
-        def get_object(self, pk):
-            try:
-              return Song.objects.get(pk=pk)
-            except Song.DoesNotExist:
-                raise Http404
+class SongDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Song.objects.get(pk=pk)
+        except Song.DoesNotExist:
+            raise Http404
             
-        def get(self, request, pk):
+    def get(self, request, pk):
             song = self.get_object(pk)
             serializer = SongSerializer(song)
             return Response(serializer.data)
-        
-        def put(self, request, pk):
+            
+    def put(self, request, pk):
             song = self.get_object(pk)
             serializer = SongSerializer(song, data=request.data)
             if serializer.is_valid():
                 serializer.save()
                 return Response(serializer.data)
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        
-        def delete(self, request, pk):
+            
+    def delete(self, request, pk):
             song = self.get_object(pk)
             song.delete()
             return Response(status=status.HTTP_204_NO_CONTENT)
