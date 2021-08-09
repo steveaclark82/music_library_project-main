9cb6d7c35fe6ff6477940b6686476f5329e2ff3d
Steve Clark <steve.a.clark48@gmail.com>
2021-08-09 14:35:43 -0500

    Fixed spelling

--- music_library/music/views.py
+++ music_library/music/views.py
@@ -23,7 +23,7 @@ class SongList(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
-    class SongDetails(APIView):
+    class SongDetail(APIView):
         def get_object(self, pk):
             try:
               return Song.objects.get(pk=pk)
