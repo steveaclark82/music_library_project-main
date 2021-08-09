72c44b4de41b8bf422c83f03f512ec366bb36646
Steve Clark <steve.a.clark48@gmail.com>
2021-08-09 12:26:59 -0500

    Created get function for SongList

--- music_library/music/views.py
+++ music_library/music/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Song
+from .serializers import SongSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
 
 # Create your views here.
+class SongList(APIView):
+    
+    def get(self, request):
+        song = Song.objects.all()
+        serializer = SongSerializer(song, many=True)
+        return Response(serializer.data)
