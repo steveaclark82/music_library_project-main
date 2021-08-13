6114d01bb105494417e670028753c9bd742ef76b
Steve Clark <steve.a.clark48@gmail.com>
2021-08-13 09:45:31 -0500

    Setup axios and cors

--- music_library/music_library/settings.py
+++ music_library/music_library/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'music'
+    'music',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware', 
 ]
 
 ROOT_URLCONF = 'music_library.urls'
@@ -131,3 +133,5 @@ try:
     from music_library.local_settings import *
 except ImportError:
     pass
+
+CORS_ORIGIN_ALLOW_ALL=True
